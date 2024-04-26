1914d8b77962ae663ce88558d869b2c3b5692109
D0hwQ1 <d0hwq1@gmail.com>
2024-04-26 18:04:21 +0900

    feat: test image-resizing func on local

---
+++ index-local.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import { imageResize } from "./index.js";
+import eventJson from "./form/OriginResponseEvent.json" assert { type: "json" };
+import fs from "fs";
+
+export const imageResizeToFile = async (event, context) => {
+  console.log("imageResizeToFile start!!");
+  event = eventJson;
+  const extension = eventJson.Records[0].cf.request.uri.split(".").pop();
+
+  console.log("===============================");
+  const response = await imageResize(event, context);
+  console.log("===============================");
+
+  const buf = new Buffer(response.body, "base64");
+  fs.writeFile(`output/image.${extension}`, buf, function (err, result) {
+    if (err) {
+      console.log("error", err);
+    }
+  });
+
+  console.log("imageResizeToFile end!!");
+};
